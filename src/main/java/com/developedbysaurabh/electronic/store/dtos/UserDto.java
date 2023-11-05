376b742cad915fdfcbc31c67ad7c746692054a42
Saurabh Shiradkar <saurabhshiradkar@gmail.com>
2023-11-05 14:48:45 +0530

    Done Changes in User dtos

--- src/main/java/com/developedbysaurabh/electronic/store/dtos/UserDto.java
+++ src/main/java/com/developedbysaurabh/electronic/store/dtos/UserDto.java
@@ -1,11 +1,11 @@
 package com.developedbysaurabh.electronic.store.dtos;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -17,14 +17,16 @@ import java.util.Set;
 public class UserDto {
 
 
+    @Schema( name = "userId", required = true,accessMode = Schema.AccessMode.READ_ONLY, description = "USER ID ")
     private String userId;
 
+
     @Size(min = 3, max = 30, message = "Invalid Name !")
-    @Schema( name = "username", required = true,accessMode = Schema.AccessMode.READ_ONLY, description = "user name of new user !!")
     private String name;
 
-//    @Email(message = "Invalid User Email !")
-    @Pattern(regexp = "^[a-z0-9][-a-z0-9._]+@([-a-z0-9]+\\.)+[a-z]{2,5}$",message = "Invalid User Email!")
+//    @Schema( name = "email", required = true,accessMode = Schema.AccessMode.READ_ONLY, description = "email of new user !!")
+//    @Pattern(regexp = "^[a-z0-9][-a-z0-9._]+@([-a-z0-9]+\\.)+[a-z]{2,5}$",message = "Invalid User Email!")
+    @Email(message = "Invalid User Email !")
     @NotBlank(message = "Email is Required!")
     private String email;
 
@@ -40,7 +42,9 @@ public class UserDto {
 
     //Custom  Validator
     //@ImageNameValid
+    @Schema( name = "imageName", required = true,accessMode = Schema.AccessMode.READ_ONLY, description = "IMAGE NAME ")
     private String imageName;
 
+    @Schema( name = "roles", required = true,accessMode = Schema.AccessMode.READ_ONLY, description = "ROLES ")
     private Set<RoleDto> roles = new HashSet<>();
 }
